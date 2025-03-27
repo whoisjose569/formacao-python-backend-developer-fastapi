4a21745d13dc5356176afffa15f40cd8a8bee341
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-03-27 15:31:51 -0300

    correcting some errors

--- src/database.py
+++ src/database.py
@@ -1,8 +1,13 @@
+import os
 import sqlalchemy as sa
 import databases
 
-DATABASE_URL = "sqlite:///./blog.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
 
 database = databases.Database(DATABASE_URL)
 metadata = sa.MetaData()
-engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+if os.getenv("RENDER"):
+    engine = sa.create_engine(DATABASE_URL)
+else:
+    engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
